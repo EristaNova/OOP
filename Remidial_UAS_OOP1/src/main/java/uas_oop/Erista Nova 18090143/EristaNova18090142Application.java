64cfc917d26254659de986ae58ed626ea6cded22
Erista Nova Saputri <59550331+EristaNova@users.noreply.github.com>
2020-08-03 12:14:44 +0700

    Add files via upload

---
+++ Remidial_UAS_OOP1/src/main/java/uas_oop/Erista Nova 18090143/EristaNova18090142Application.java
@@ -0,0 +1,13 @@
+package uas_oop.erista_18090143;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Adesug18090064Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Adesug18090064Application.class, args);
+	}
+
+}
---
+++ Remidial_UAS_OOP1/src/test/java/uas_oop/EristaNova_18090143/EristaNova18090143ApplicationTests.java
@@ -0,0 +1,13 @@
+package uas_oop.erista_18090143;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Adesug18090064ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
